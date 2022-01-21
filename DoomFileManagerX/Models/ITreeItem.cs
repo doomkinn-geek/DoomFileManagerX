001104aba00609dacf15d200cbe1a8a6e19d0a6e
doomkinn@yandex.ru <doomkinn@yandex.ru>
2022-01-21 16:14:49 +0300

    tree items added

---
+++ DoomFileManagerX/Models/ITreeItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoomFileManagerX.Models
+{
+    public interface ITreeItem : INotifyPropertyChanged
+    {
+        // Короткое имя отображаемое в дереве
+        string VisibleName { get; set; }
+        string FullPathName { get; set; }
+
+        ObservableCollection<ITreeItem> Children { get; }
+
+        bool IsExpanded { get; set; }
+
+        bool IncludeFileChildren { get; set; }
+
+        // Для сброса дерева
+        void DeleteChildren();
+    }
+}
