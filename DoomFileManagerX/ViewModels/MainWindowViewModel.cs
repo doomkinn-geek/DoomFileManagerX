6c4e4fe9bc8eae615b49e9dccfccd268aa28251d
doomkinn@yandex.ru <doomkinn@yandex.ru>
2022-01-21 16:23:18 +0300

    main view model started

--- DoomFileManagerX/ViewModels/MainWindowViewModel.cs
+++ DoomFileManagerX/ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,19 @@
+using DoomFileManagerX.Models;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 
 namespace DoomFileManagerX.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        public string Greeting => "Welcome to Avalonia!";
+        public string WindowHeader => "Cross-platform version of File Manager";
+        private ObservableCollection<ITreeItem> rootChildren = new ObservableCollection<ITreeItem> { };
+        public ObservableCollection<ITreeItem> RootChildren
+        {
+            get => rootChildren;
+            set => rootChildren = value;
+        }
     }
 }
